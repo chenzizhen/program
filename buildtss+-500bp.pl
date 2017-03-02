af13c69f016e4a568e7c08c67193bbabb303a61c
chenzizhen <1375952819@qq.com>
2017-03-02 15:09:09 +0800

    Create buildtss+-500bp.pl

---
+++ buildtss+-500bp.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+use autodie;
+use strict;
+open REFFLAT_FH,'<','/Volumes/myMAC/asxl1chipseq/bed/refFlat.txt' or die "$!";
+open REF_FH,'>>','/Volumes/myMAC/asxl1chipseq/bed/tss+-500.cod' or die "$!";
+print REF_FH "#id\tchrom\tstart\tend\tstrand\n";
+while (my $lines = <REFFLAT_FH>){
+    chomp($lines);
+    my @lines = split"\t",$lines;
+    if($lines[3] eq '+'){printf REF_FH "%s\t%s\t%d\t%d\t%s\n",$lines[0],$lines[2],$lines[4]-500,$lines[4]+500,$lines[3];}
+    if($lines[3] eq '-'){printf REF_FH "%s\t%s\t%d\t%d\t%s\n",$lines[0],$lines[2],$lines[5]-500,$lines[5]+500,$lines[3];}
+}
+close REFFLAT_FH;
+close REF_FH;
