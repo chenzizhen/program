eab7df90d9f619e83a866fb2eb759974783cc841
chenzizhen <1375952819@qq.com>
2019-10-14 10:47:39 +0800

    Create 分组+选病人-生存曲线.R

---
+++ 分组+选病人-生存曲线.R
@@ -0,0 +1,21 @@
+load("c6RData")
+H <- coef(result[[3]])
+survival <- read.delim("~/Desktop/czz/生信文章/beataml/survival1.txt", stringsAsFactors=FALSE)
+H <- t(H)
+allpatient<-data.frame(rownames(H))
+colnames(allpatient)<-c("ID")
+#建立所有样品生存曲线和survival矩阵i
+allsurvival <- merge(allpatient,survival,by.x="ID",by.y="LabId")
+ID<-as.character(allsurvival$ID)
+time<-as.integer(allsurvival$time)
+status<-as.integer(allsurvival$status)
+group=result$group
+distanceMatrix<-result$distanceMatrix
+p_value=survAnalysis(mainTitle = "AML",time,status,group,distanceMatrix,similarity = 1)
+i<-cbind.data.frame(ID,time,status,group,stringsAsFactors=0)
+FSpatient <- read.csv("./patients/a.txt", sep="")
+i1<-merge(FSpatient,i,by.x="LabId",by.y="ID")
+time1<-as.integer(i1[,2])
+status1<-as.integer(i1[,3])
+group1<-as.integer(i1[,4])
+p_value=survAnalysis(mainTitle = "AML",time1,status1,group1,distanceMatrix,similarity = 1)
